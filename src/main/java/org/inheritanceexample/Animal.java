e589d3a59484c91298265127b9d93d34c656a01e
youngmetroid <outsetwaker@gmail.com>
2026-07-15 11:56:24 -0600

    created a inheritance example

---
+++ src/main/java/org/inheritanceexample/Animal.java
@@ -0,0 +1,10 @@
+package org.inheritanceexample;
+
+public interface Animal {
+    int getLegs();
+    void sound();
+    float height();
+    int teeth();
+    String getName();
+    void print();
+}
